8dc094cfa540caabde4142290cde3906ea5e4e25
Allan Bogh <ajbogh@allanbogh.com>
2015-01-14 23:35:57 -0800

    Added test for removing entries

--- test.js
+++ test.js
@@ -9,6 +9,15 @@ it('applies glob and returns a flat array with no duplicates', function (done){
   });
 });
 
+it('removes the correct entries', function (done){
+  flatGlob(['*.js', '*.md', '!README.md'], function (error, files) {
+    if (error) return done(error);
+
+    expect(files).to.deep.equal(['index.js', 'test.js']);
+    done();
+  });
+});
+
 it('runs synchronously optionally', function(){
   expect(flatGlob.sync(['index.js', '*.js'])).to.deep.equal(['index.js', 'test.js']);
 });
