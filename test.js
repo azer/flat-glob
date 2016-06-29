842ce7f923e6db90d778544ad707e5048a2ef62c
9ab62cf36bcecb2f07d0b948fa3ce74288a6697d <keli.grubb@spinen.com>
2016-06-29 11:59:43 -0400

    cleared up the package.json and added non-wildcard test

--- test.js
+++ test.js
@@ -10,10 +10,15 @@ it('applies glob and returns a flat array with no duplicates', function (done){
   });
 });
 
-it('runs synchronously optionally', function(){
-  expect(flatGlob.sync(['index.js', '*.js'])).to.deep.equal(['index.js', 'test.js']);
+it('tests without wildcard', function (done){
+  flatGlob(['index.js', 'test.js', 'test.js'], function (error, files) {
+    if (error) return done(error);
+
+    expect(files).to.deep.equal(['index.js', 'test.js']);
+    done();
+  });
 });
 
 it('runs synchronously optionally', function(){
-  expect(flatGlob.sync(['*.js', '*.md', '!README.md'])).to.deep.equal(['index.js', 'test.js']);
+  expect(flatGlob.sync(['index.js', '*.js'])).to.deep.equal(['index.js', 'test.js']);
 });
